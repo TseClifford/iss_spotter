c8066d239b2a2eb24be4e5f79a38ec4cacb29450
Clifford Tse <TseClifford@gmail.com>
2022-01-20 22:18:54 +0000

    added initial fetch ip function

--- iss.js
+++ iss.js
@@ -1,1 +1,20 @@
+const request = require('request');
 
+const fetchMyIP = function(callback) {
+  const url = 'https://api.ipify.org?format=json';
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      callback(`Status Code ${response.statusCode} when fetching IP. Response: ${body}`, null);
+      return;
+    }
+    
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
+
+module.exports = { fetchMyIP };
